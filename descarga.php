684ff151f3d9ff9c78129264f0bdd18b50896177
Tsolete <46019970+Tsolete@users.noreply.github.com>
2018-12-20 12:49:19 +0100

    Añadida la fecha y el enlace al foro

--- descarga.php
+++ descarga.php
@@ -1,10 +1,10 @@
 ﻿<img alt="descargas" src="imagenes/descargas.png" align="right" width="60%" />
 <h2>Versión actual</h2>
-<h3>Final Fight LNS 3.0</h3>
+<h3>Final Fight LNS 3.0 (versión 20181214)</h3>
 <a href="contador.php" target="_blank"><img alt="Mega" src="imagenes/mega.png" width="5%" /></a>
 <a href="magnet:?xt=urn:btih:0AE22794DEA8C7AD43D598319C010A92B803D9B9&dn=Final%20Fight%20LNS%203.0.7z&tr=udp%3a%2f%2ftracker.openbittorrent.com%3a80%2fannounce&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce"><img alt="Magnet" src="imagenes/magnet.jpg" width="5%" /></a>
 <br>
-<a style="color:black" href="http://nochesskyperas.1foro.com/descarga-f42.html">(ejecutables de windows, para otras plataformas revisar el foro.)</a>
+<p><a style="color:black" href="http://nochesskyperas.1foro.com/descarga-f42.html">(ejecutables de windows, para otras plataformas revisar el foro.)</p></a>
 <br>
 
 <h3>BSO Final Fight LNS 3.0</h3>
